2f6e5e5d13b529671e1b7b5cd02275ef9bb6119e
Brian Hensley <brnhensley@gmail.com>
2021-09-09 14:29:10 -0700

    files renamed CretaceousApi

---
+++ CretaceousApi/Controllers/AnimalsController.cs
@@ -0,0 +1,34 @@
+﻿using CretaceousApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CretaceousApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private CretaceousApiContext _db;
+
+    public AnimalsController(CretaceousApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+  }
+}
