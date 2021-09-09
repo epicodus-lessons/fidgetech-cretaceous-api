783a838925b3384a44a40c6e5eadf2912e40c623
Brian Hensley <brnhensley@gmail.com>
2021-09-09 14:48:22 -0700

    3.6.0.05 - API: Create and Read

--- CretaceousApi/Controllers/AnimalsController.cs
+++ CretaceousApi/Controllers/AnimalsController.cs
@@ -1,34 +1,51 @@
 ﻿using CretaceousApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace CretaceousApi.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AnimalsController : ControllerBase
-  {
-    private CretaceousApiContext _db;
-
-    public AnimalsController(CretaceousApiContext db)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
     {
-      _db = db;
-    }
+        private CretaceousApiContext _db;
 
-    // GET api/animals
-    [HttpGet]
-    public ActionResult<IEnumerable<Animal>> Get()
-    {
-      return _db.Animals.ToList();
-    }
+        public AnimalsController(CretaceousApiContext db)
+        {
+            _db = db;
+        }
 
-    // POST api/animals
-    [HttpPost]
-    public void Post([FromBody] Animal animal)
-    {
-      _db.Animals.Add(animal);
-      _db.SaveChanges();
+        // GET api/animals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> Get()
+        {
+            return await _db.Animals.ToListAsync();
+        }
+
+        // POST api/animals
+        [HttpPost]
+        public async Task<ActionResult<Animal>> Post(Animal animal)
+        {
+            _db.Animals.Add(animal);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+        }
+
+        // GET api/animals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+            Animal animal = await _db.Animals.FindAsync(id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return animal;
+        }
     }
-  }
 }
